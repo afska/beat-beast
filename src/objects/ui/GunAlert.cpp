4f0ddc50c3eaccc88e5feaa9ae78c34746d5bf26
Rodrigo Alfonso <rodri042@gmail.com>
2024-08-14 00:45:00 -0300

    Making gun alerts more noticeable

--- src/objects/ui/GunAlert.cpp
+++ src/objects/ui/GunAlert.cpp
@@ -13,8 +13,15 @@ GunAlert::GunAlert(bn::sprite_item sprite, bn::fixed_point _topLeftPosition)
 
   mainSprite.set_z_order(-1);
   mainSprite.set_bg_priority(0);
+  mainSprite.set_scale(2);
 }
 
 void GunAlert::update() {
   animation.update();
+
+  if (mainSprite.horizontal_scale() > 1) {
+    mainSprite.set_scale(mainSprite.horizontal_scale() - 0.005);
+    if (mainSprite.horizontal_scale() < 1)
+      mainSprite.set_scale(1);
+  }
 }
